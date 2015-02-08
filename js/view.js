1348c7f7a4a3a2fe5263b571f8cd768e0fd97952
Eric Kennedy <ekeric13@gmail.com>
2015-02-07 18:42:11 -0700

    Blog update at 2015-02-08 01:42:11

---
+++ js/view.js
@@ -0,0 +1,9 @@
+function View() {
+    this.elements= function() {
+        $("#song-form").hide();
+        $("#song-button").hide();
+        $("#your-song").hide();
+        $("#song-list").hide();
+
+    }
+}
